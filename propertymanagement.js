a98254ba3f43962bf12d4e85ea84e380fe2baa59
Manisha3009 <162792985+Manisha3009@users.noreply.github.com>
2024-11-29 20:59:28 +0530

    Add files via upload

---
+++ propertymanagement.js
@@ -0,0 +1,59 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import getProperty from '@salesforce/apex/PropertHandler_LWC.getProperty';
+import { getRecord } from 'lightning/uiRecordApi';
+import USER_ID from '@salesforce/user/Id';
+
+export default class PropertyManagement extends LightningElement {
+  @api recordId;
+  userId = USER_ID;
+  verifiedvar;
+  typevar;
+  isfalse = true;
+  istrue = false;
+  @track propertylist = [];
+
+  columns = [
+    { label: 'Property Name', fieldName: 'Property_Name__c' },
+    { label: 'Property Type', fieldName: 'Type__c' },
+    { label: 'Property Location', fieldName: 'Location__c' },
+    { label: 'Property link', fieldName: 'Property_link__c' }
+  ];
+
+  propetyoptions = [
+    { label: 'Commercial', value: 'Commercial' },
+    { label: 'Residential', value: 'Residential' },
+    { label: 'Rental', value: 'Rental' }
+  ];
+
+  @wire(getRecord, { recordId: '$userId', fields: ['User.Verified__c'] })
+  recordFunction({ data, error }) {
+    if (data) {
+      console.log('This is the User Id ---> ' + this.userId);
+      this.verifiedvar = data.fields.Verified__c.value;
+    } else {
+      console.error(error);
+      console.log('This is error');
+    }
+  }
+
+  changehandler(event) {
+    this.typevar = event.target.value;
+  }
+
+  handleClick() {
+    getProperty({ type: this.typevar, verified: this.verifiedvar })
+      .then((result) => {
+        this.isfalse = true;
+        if (result && result.length > 0) {
+          this.istrue = true;
+          this.propertylist = result;
+        } else {
+          this.isfalse = false;
+          this.istrue = false;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
